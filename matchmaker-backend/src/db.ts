16fab5b114cea882d164e73158cff7f1b8565f5c
ref-yu <ainoe88224646baba@gmail.com>
2026-05-13 03:21:44 +0800

    fix: 切换数据库为PostgreSQL

--- matchmaker-backend/src/db.ts
+++ matchmaker-backend/src/db.ts
@@ -1,3 +1,6 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-export const db = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+export const db = new PrismaClient({ adapter });
